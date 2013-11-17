4ba47164b2b36983310111580373f4190a7778ae
Rex <rex@kittykatattack.com>
2013-11-17 16:48:15 -0500

    fixed grade 5 template

--- script.js
+++ script.js
@@ -90,6 +90,31 @@
     //Replace the  [quality]
     //anecdote = anecdote.split("[quality]").join(quality);
     
+    if (quality === "1") {
+      anecdote = anecdote.split("[an excellent, a very good, a good]").join("an excellent");
+      anecdote = anecdote.split("[are well developed, are developing well, are developing]").join("are well developed");
+      anecdote = anecdote.split("[a highly imaginative, an imaginative, an interesting]").join("a highly imaginative");
+      anecdote = anecdote.split("[very well constructed, well constructed, acceptably constructed]").join("very well constructed");
+      anecdote = anecdote.split("[an excellent, a very good, a good]").join("an excellent");
+      anecdote = anecdote.split("[produced artwork of a high standard and [she] has shown an impressive commitment, produced good quality work and [her] art skills will continue to improve with practise, shows an ability in art but should remember to maintain focus when in class]").join("produced artwork of a high standard and [she] has shown an impressive commitment");
+    }
+    if (quality === "2") {
+      anecdote = anecdote.split("[an excellent, a very good, a good]").join("a very good");
+      anecdote = anecdote.split("[are well developed, are developing well, are developing]").join("are developing well");
+      anecdote = anecdote.split("[a highly imaginative, an imaginative, an interesting]").join("an imaginative");
+      anecdote = anecdote.split("[very well constructed, well constructed, acceptably constructed]").join("well constructed");
+      anecdote = anecdote.split("[an excellent, a very good, a good]").join("a very good");
+      anecdote = anecdote.split("[produced artwork of a high standard and [she] has shown an impressive commitment, produced good quality work and [her] art skills will continue to improve with practise, shows an ability in art but should remember to maintain focus when in class]").join("produced good quality work and [her] art skills will continue to improve with practise");
+    }
+    if (quality === "3") {
+      anecdote = anecdote.split("[an excellent, a very good, a good]").join("a good");
+      anecdote = anecdote.split("[are well developed, are developing well, are developing]").join("are developing");
+      anecdote = anecdote.split("[a highly imaginative, an imaginative, an interesting]").join("an interesting");
+      anecdote = anecdote.split("[very well constructed, well constructed, acceptably constructed]").join("acceptably constructed");
+      anecdote = anecdote.split("[an excellent, a very good, a good]").join("a good understanding");
+      anecdote = anecdote.split("[produced artwork of a high standard and [she] has shown an impressive commitment, produced good quality work and [her] art skills will continue to improve with practise, shows an ability in art but should remember to maintain focus when in class]").join("shows an ability in art but should remember to maintain focus when in class");
+    }
+    
     if (gender.charAt(0) === "b") {
       anecdote = anecdote.split("to [her]").join("to him");
       anecdote = anecdote.split("[her]").join("his");
@@ -110,31 +135,6 @@
       anecdote = anecdote.split("[Her]").join("Her");
       anecdote = anecdote.split("[she]").join("she");
       anecdote = anecdote.split("[She]").join("She");
-    }
-    
-    if (quality === "1") {
-      anecdote = anecdote.split("[an excellent, a very good, a good]").join("an excellent");
-      anecdote = anecdote.split("[are well developed, are developing well, are developing]").join("are well developed");
-      anecdote = anecdote.split("[a highly imaginative, an imaginative, an interesting]").join("a highly imaginative");
-      anecdote = anecdote.split("[very well constructed, well constructed, acceptably constructed]").join("very well constructed");
-      anecdote = anecdote.split("[an excellent, a very good, a good]").join("an excellent");
-      anecdote = anecdote.split("[produced artwork of a high standard and showed an impressive commitment, produced good quality work and will continue to improve with practise, shows an ability in art but should remember to maintain focus when in class]").join("produced artwork of a high standard and showed an impressive commitment");
-    }
-    if (quality === "2") {
-      anecdote = anecdote.split("[an excellent, a very good, a good]").join("a very good");
-      anecdote = anecdote.split("[are well developed, are developing well, are developing]").join("are developing well");
-      anecdote = anecdote.split("[a highly imaginative, an imaginative, an interesting]").join("an imaginative");
-      anecdote = anecdote.split("[very well constructed, well constructed, acceptably constructed]").join("well constructed");
-      anecdote = anecdote.split("[an excellent, a very good, a good]").join("a very good");
-      anecdote = anecdote.split("[produced artwork of a high standard and showed an impressive commitment, produced good quality work and will continue to improve with practise, shows an ability in art but should remember to maintain focus when in class]").join("produced good quality work and will continue to improve with practise");
-    }
-    if (quality === "3") {
-      anecdote = anecdote.split("[an excellent, a very good, a good]").join("a good");
-      anecdote = anecdote.split("[are well developed, are developing well, are developing]").join("are developing");
-      anecdote = anecdote.split("[a highly imaginative, an imaginative, an interesting]").join("an interesting");
-      anecdote = anecdote.split("[very well constructed, well constructed, acceptably constructed]").join("acceptably constructed");
-      anecdote = anecdote.split("[an excellent, a very good, a good]").join("a good understanding");
-      anecdote = anecdote.split("[produced artwork of a high standard and showed an impressive commitment, produced good quality work and will continue to improve with practise, shows an ability in art but should remember to maintain focus when in class]").join("shows an ability in art but should remember to maintain focus when in class");
     }
     
     //Build the anecdote
